9d245b7578bcf2261162d29f74d7b09088db61db
Dinastía Harzu αγρ <111463709+Dinastia-Harzu@users.noreply.github.com>
2024-03-21 18:21:38 +0100

    Crear back-end

---
+++ backend/service/RegistrarUsuarioService.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+/**
+ *
+ * no response value expected for this operation
+ **/
+exports.usuariosPOST = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
