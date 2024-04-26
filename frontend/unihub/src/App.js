9c0f7a3ecd9986206f66f00550e2e7f2cc51e53a
PaulaLA22 <pll43@alu.ua.es>
2024-04-26 20:41:18 +0200

    Header y footer medio acabado

--- frontend/unihub/src/App.js
+++ frontend/unihub/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "./styles/general.css"
 import Header from "./components/commons/Header.js"
+import Footer from "./components/commons/Footer.js"
+import Main from "./components/commons/Main.js"
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
