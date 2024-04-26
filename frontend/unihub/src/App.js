842fdbd67aae1e5600e4c608205402d7d339ee2f
Ainhoa <apap4@alu.ua.es>
2024-04-26 20:46:15 +0200

    Formulario de los datos del usuario

--- frontend/unihub/src/App.js
+++ frontend/unihub/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import "./styles/general.css"
 import Header from "./components/commons/Header.js"
-
+import Perfilusuario from "./components/Perfilusuario.js"
 export default function App() {
   return (
     <div>
       <Header />
+      <Perfilusuario />
     </div>
   );
 }
