1ed32f6a7aab912596901801e61096ef0f5a1119
PaulaLA22 <pll43@alu.ua.es>
2024-04-26 18:42:51 +0200

    Empezando vamoooooooo se acaba aaaaaaaaaaaaaaaaa AAAa

--- frontend/unihub/src/App.js
+++ frontend/unihub/src/App.js
@@ -1,15 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./styles/general.css"
+import Header from "./components/commons/Header.js"
 
 export default function App() {
   return (
-    <div className="App-header">
-      <MiBoton img={logo} />
+    <div>
+      <Header />
     </div>
   );
-}
-
-function MiBoton(obj) {
-  console.log(obj);
-  return <img src={obj.img} className="App-logo" alt="logo" />;
 }
---
+++ frontend/unihub/src/components/commons/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="navbar is-primary">
+      <div className='navbar-brand'>
+        <a className='navbar-item'>
+          <img class='logotipo' src="assets/Tiny_Logo.PNG" />
+        </a>
+      </div>
+    </div>
+  );
+}
