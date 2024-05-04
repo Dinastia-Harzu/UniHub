025ad49c374e889a003fc513549449d347b0d9c2
PaulaLA22 <pll43@alu.ua.es>
2024-05-04 18:48:20 +0200

    Probando burguer menu

--- frontend/unihub/src/components/commons/Header.js
+++ frontend/unihub/src/components/commons/Header.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu'
 
 export default function Header() {
   return (
-    <div className="navbar">
-      <div className="container1">
-        <a href="/">
-          <img className='logotipo' src="assets/Tiny_Logo.PNG" alt="logo" />
-        </a>
-      </div>
-      <div class="container2">
-        <ul className="navbar-menu">
-          <li className="navbar-item"><a href="/">Inicio</a></li>
-          <li className="navbar-item"><a href="/descubrir">Descubrir</a></li>
-          <li className="navbar-item"><a href="/buscar">Buscar</a></li>
-        </ul>
-      </div>
-      <div class="container3">
-        <div className="navbar-buttons">
-          <a className="btn btn-secondary" href="/login">Identificarse</a>
-          <a className="btn btn-primary" href="/signup">Registrarse</a>
+    <header>
+      <div className="navbar">
+        <div className="container1">
+          <a href="/">
+            <img className='logotipo' src="assets/Tiny_Logo.PNG" alt="logo" />
+          </a>
         </div>
+        <Menu pageWrapId={"page-wrap"} left>
+          <div class="container2">
+            <ul className="navbar-menu">
+              <li className="navbar-item"><a href="/">Inicio</a></li>
+              <li className="navbar-item"><a href="/descubrir">Descubrir</a></li>
+              <li className="navbar-item"><a href="/buscar">Buscar</a></li>
+            </ul>
+          </div>
+          <div class="container3">
+            <div className="navbar-buttons">
+              <a className="btn btn-secondary" href="/login">Identificarse</a>
+              <a className="btn btn-primary" href="/signup">Registrarse</a>
+            </div>
+          </div>
+        </Menu>
       </div>
-    </div>
+    </header>
   );
 }
