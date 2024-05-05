53a2914434faaa40a943d2785a0be560b21e0375
PaulaLA22 <pll43@alu.ua.es>
2024-05-05 20:03:49 +0200

    Página de búsqueda maquetada

--- frontend/unihub/src/App.js
+++ frontend/unihub/src/App.js
@@ -3,12 +3,15 @@ import Header from "./components/commons/Header.js";
 import Inicio from "./components/Inicio.js";
 import Footer from "./components/commons/Footer.js";
 import Perfilusuario from "./components/Perfilusuario.js";
+import Busqueda from "./components/Busqueda.js";
+
 export default function App() {
   return (
     <div>
       <Header />
-      <Perfilusuario />
-      <Inicio />
+      <Busqueda />
+      {/* <Perfilusuario /> */}
+      {/* <Inicio /> */}
       <Footer />
     </div>
   );
