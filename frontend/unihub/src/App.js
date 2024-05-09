517780367ba40cf126d93c588c9bb6a08e9170a7
Ainhoa <apap4@alu.ua.es>
2024-05-09 12:49:59 +0200

    advances en el formulario

--- frontend/unihub/src/App.js
+++ frontend/unihub/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
     <div>
       <Header />
       <Busqueda />
-      {/* <Perfilusuario /> */}
+       <Perfilusuario /> 
       {/* <Inicio /> */}
       <Footer />
     </div>
