8b25c0a98beffca679e1f7c87ebfe9e12d376c48
Dinastía Harzu αγρ <111463709+Dinastia-Harzu@users.noreply.github.com>
2024-05-28 01:52:45 +0200

    Mejorar `Tiempo`

--- frontend/unihub/src/components/commons/Tiempo.js
+++ frontend/unihub/src/components/commons/Tiempo.js
@@ -1,19 +1,36 @@
-export default function Tiempo(params = {}) {
-  console.log(params);
+export default function Tiempo({
+  fechaHora = Date(),
+  formato = null,
+  local = "es",
+}) {
   return (
-    <time dateTime={params.fechaHora}>
-      {params.formato
-        ? new Date(params.fechaHora).toLocaleDateString()
-        : fechaFormateada(params.fechaHora, params.local)}
+    <time dateTime={fechaHora}>
+      {fechaFormateada(fechaHora, local, formato)}
     </time>
   );
 }
 
-function fechaFormateada(fecha_hora, local) {
-  console.info(fecha_hora);
-  return new Intl.DateTimeFormat(local, {
+function fechaFormateada(fecha_hora, local, formato) {
+  const config = {
     year: "numeric",
     month: "long",
     day: "numeric",
-  }).format(new Date(fecha_hora));
+  };
+  switch (formato) {
+    case "DD/MM/YYYY":
+      config.month = "2-digit";
+      config.day = "2-digit";
+      break;
+    case "DD/MM/YY":
+      config.year = "2-digit";
+      config.month = "2-digit";
+      config.day = "2-digit";
+      break;
+    case "YYYY":
+      delete config.month;
+      delete config.day;
+    default:
+      break;
+  }
+  return new Intl.DateTimeFormat(local, config).format(new Date(fecha_hora));
 }
