b8034af35535405f5a784cb58ced4c32cfcf9b10
Marta Gómez <martagomver2003@hotmail.com>
2024-05-29 17:46:37 +0200

    Children en root

--- frontend/unihub/src/components/Root.js
+++ frontend/unihub/src/components/Root.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "./commons/Header";
 import Footer from "./commons/Footer";
 
-export default function Root() {
+export default function Root({ children }) {
   return (
     <>
       <Header />
+      {children}
       <Footer />
     </>
   );
