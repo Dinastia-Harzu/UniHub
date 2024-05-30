58b42fc9580cce448f9af04d1d0892305273ff78
Dinastía Harzu αγρ <111463709+Dinastia-Harzu@users.noreply.github.com>
2024-05-30 03:10:10 +0200

    Quitar div innecesario

--- frontend/unihub/src/components/NotFound.js
+++ frontend/unihub/src/components/NotFound.js
@@ -9,11 +9,9 @@ export default function NotFound() {
       <div className="error-container">
         <h1 className="error-title titulo-letra">404</h1>
         <p className="error-message contenido-letra">{t("pag-nf")}</p>
-        <div className="btn-letra">
-          <Link to="/" className="btn home-link btn-letra">
-            {t("btn-volver2")}
-          </Link>
-        </div>
+        <Link to="/" className="btn home-link btn-letra">
+          {t("btn-volver2")}
+        </Link>
       </div>
     </main>
   );
